98db6761cb98cb617fc286001d84b7ed665b6dfa
Jan Duinkerken Rodríguez <jan.duinkerken@sngular.com>
2024-04-17 15:57:32 +0200

    Parse let statements

---
+++ ast/ast.go
@@ -0,0 +1,52 @@
+package ast
+
+import (
+	"monkey/m/token"
+)
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {
+	// mock function to help compiler with type checking
+}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {
+	// mock function to help compiler with type checking
+}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
