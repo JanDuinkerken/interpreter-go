00ae9e585da8f789ec6d4ab6817fd8dae6c56eb9
Jan Duinkerken Rodríguez <jan.duinkerken@sngular.com>
2024-04-22 18:08:45 +0200

    Environment and bindngs

--- repl/repl.go
+++ repl/repl.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"monkey/m/evaluator"
 	"monkey/m/lexer"
+	"monkey/m/object"
 	"monkey/m/parser"
 )
 
@@ -13,6 +14,7 @@ const PROMPT = ">>"
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Fprint(out, PROMPT)
@@ -31,7 +33,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
